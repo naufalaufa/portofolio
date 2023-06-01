23b1cef0bd74bff3746caea5e27b29cf21e18c55
naufal aufa <muhammadnaufalaufarifqi@gmail.com>
2023-06-01 17:05:55 +0700

    delete build

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -6,7 +6,9 @@ function Main() {
       <h1 className="text-white text-4xl font-bold sm:text-5xl">
         Hi! , Welcome To My Portfolio,
       </h1>
-      <div className="text-white text-xl font-bold "></div>
+      <div className="text-white text-xl font-bold ">
+        <p>Lets enjoy for the experience</p>
+      </div>
       <a
         href="#abouts"
         className="hover:bg-white duration-150 text-2xl font-bold text-black bg-yellow-200 p-4 rounded-xl mt-10 absolute top-[400px] sm:top-[350px] md:top-[310px]"
